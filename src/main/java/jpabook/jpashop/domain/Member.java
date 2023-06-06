0e9dfb4b5c8ae78b5753a4f261515ee4a55c0c03
ShinHeeJin <sisesu92@gmail.com>
2023-06-06 23:35:31 +0900

    feat : getter setter

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,4 +15,28 @@ public class Member {
 
     @Column(name = "USERNAME")
     private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
