b158bf778a1e44a49939c576eaef6ac7d9fae28c
ShinHeeJin <sisesu92@gmail.com>
2023-06-06 23:17:23 +0900

    fix : MemberTeam - MemberEntity

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -10,48 +10,9 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
-    private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+    @Column(name = "USERNAME")
+    private String username;
 }
