dd3c96076dcb824bdd2c506575bd780a6cbb8feb
ShinHeeJin <sisesu92@gmail.com>
2023-07-27 01:12:49 +0900

    Create Base Entities

---
+++ src/main/java/jpabook/jpql/Address.java
@@ -0,0 +1,35 @@
+package jpabook.jpql;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String zipcode;
+    private String street;
+    private String city;
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
