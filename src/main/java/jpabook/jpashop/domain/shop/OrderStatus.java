33494c194b9eda350be9248950d55ab7dde43da6
ShinHeeJin <sisesu92@gmail.com>
2023-07-27 01:05:01 +0900

    JPQL Project Initial Commit

--- src/main/java/jpabook/jpashop/domain/shop/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabook.jpashop.domain.shop;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
---
+++ src/main/java/jpabook/jpql/JpqlMain.java
@@ -0,0 +1,30 @@
+package jpabook.jpql;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpqlMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hellojpql");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            Member member = new Member();
+            member.setUsername("memberA");
+            member.setAge(31);
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
