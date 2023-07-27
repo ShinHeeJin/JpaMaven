1f2daf6a5767cfd00d042c20f9ce790a5b3d78bc
ShinHeeJin <sisesu92@gmail.com>
2023-07-27 23:21:57 +0900

    JPQL - theta join

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -15,12 +15,12 @@ public class JpqlMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("memberA");
+            member.setUsername("teamA");
             member.setAge(30);
             member.changeTeam(team);
             em.persist(member);
 
-            String query = "select m from Member m left outer join m.team t";
+            String query = "select m from Member m, Team t where m.username = t.name";
             List<Member> members = em.createQuery(query, Member.class).getResultList();
             for (Member each : members) {
                 System.out.println("each = " + each);
