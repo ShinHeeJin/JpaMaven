408c5d04e6a5a65dcd4694a7baf6b1e4806ab5f5
ShinHeeJin <sisesu92@gmail.com>
2023-07-27 23:05:47 +0900

    Paging

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -10,18 +10,21 @@ public class JpqlMain {
         tx.begin();
         try {
 
-            Member member = new Member();
-            member.setUsername("memberA");
-            member.setAge(31);
-            em.persist(member);
-
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from " +
-                            "Member m", MemberDTO.class)
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("memberA" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
-            MemberDTO memberDTO = resultList.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
+            System.out.println("resultList.size() = " + resultList.size());
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
